a8f24c2720c35df18202721f03f111353e8dd8bf
Magaldi2 <magaldlucas6@gmail.com>
2024-10-19 21:46:12 -0300

    corrigido

--- frontend/src/dashboardp.js
+++ frontend/src/dashboardp.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { json, Link, useNavigate } from "react-router-dom";
+import {Link, useNavigate } from "react-router-dom";
 
 function Dashboard() {
   const navigate = useNavigate();
