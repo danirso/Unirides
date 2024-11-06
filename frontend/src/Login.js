cebe21de862898bb45213c257a54ea49322c2f94
Matheus <mattanitelli4@gmail.com>
2024-11-06 18:02:49 -0300

    botão retorno landPage

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -173,6 +173,14 @@ function Login() {
           >
             Criar Conta
           </Link>
+
+          <p className="text-center mb-0">
+            retornar - {" "}
+            <Link to="/" className="text-primary">
+              Inicio
+            </Link>
+          </p>
+
         </form>
       </div>
     </div>
