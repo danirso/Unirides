20a283e66f625972aa92d489b2c4fa0412cd1286
Matheus <mattanitelli4@gmail.com>
2024-11-24 23:03:02 -0300

    design frontend definido

--- frontend/src/esquecisenha.js
+++ frontend/src/esquecisenha.js
@@ -2,46 +2,44 @@ import React, { useState } from "react";  // Importando useState
 import { useNavigate } from "react-router-dom";
 
 const RecuperacaoSenha = ({ setShowRecovery, setShowCodeVerification }) => {
-    const [email, setEmail] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
-    const [successMessage, setSuccessMessage] = useState("");
-    const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const navigate = useNavigate();
 
+  const handleSendEmail = async (e) => {
+    e.preventDefault();
 
-    const handleSendEmail = async (e) => {
-        e.preventDefault();
-      
-        // Limpa mensagens anteriores
-        setSuccessMessage('');
-        setErrorMessage('');
-      
-        try {
-          const response = await fetch('/recuperar-senha', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email }),
-          });
-      
-          const data = await response.json();
-      
-          if (response.ok) {
-            setSuccessMessage('Código enviado para o e-mail!');
-            setShowRecovery(false);  // Para avançar para a próxima etapa
-            setShowCodeVerification(true);
-          } else {
-            // Verifica se o erro é de "e-mail não encontrado"
-            if (data.error === 'E-mail não encontrado.') {
-              setErrorMessage('E-mail não encontrado! Verifique novamente.');
-            } else {
-              setErrorMessage('Ocorreu um erro. Tente novamente mais tarde.');
-            }
-          }
-        } catch (error) {
-          setErrorMessage('Erro de conexão. Tente novamente.');
+    // Limpa mensagens anteriores
+    setSuccessMessage('');
+    setErrorMessage('');
+
+    try {
+      const response = await fetch('/recuperar-senha', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setSuccessMessage('Código enviado para o e-mail!');
+        setShowRecovery(false);  // Para avançar para a próxima etapa
+        setShowCodeVerification(true);
+      } else {
+        if (data.error === 'E-mail não encontrado.') {
+          setErrorMessage('E-mail não encontrado! Verifique novamente.');
+        } else {
+          setErrorMessage('Ocorreu um erro. Tente novamente mais tarde.');
         }
-      };
+      }
+    } catch (error) {
+      setErrorMessage('Erro de conexão. Tente novamente.');
+    }
+  };
 
   return (
     <div
@@ -51,25 +49,45 @@ const RecuperacaoSenha = ({ setShowRecovery, setShowCodeVerification }) => {
         left: 0,
         width: "100%",
         height: "100%",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
-        backdropFilter: "blur(8px)",
+        background: "linear-gradient(to right, #0f2027, #203a43, #2c5364)",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         zIndex: 1000,
+        fontFamily: "Poppins, sans-serif",
       }}
     >
       <div
         style={{
-          background: "linear-gradient(to right, #0f2027, #203a43, #2c5364)",
-          padding: "20px",
-          borderRadius: "8px",
-          width: "300px",
+          backgroundColor: "#343a40",
+          padding: "40px 30px",
+          borderRadius: "12px",
+          width: "500px",
+          boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)",
+          animation: "fadeIn 1s ease-in-out",
         }}
       >
-        <h5 className="text-center" style={{ color: "#fff" }}>
-          Recuperar Senha
-        </h5>
+        <div style={{ textAlign: "center" }}>
+          <img
+            src={`${process.env.PUBLIC_URL}/logo.png`}
+            alt="UNIRIDES"
+            style={{
+              position: "absolute",
+              top: "30px",
+              left: "50%",
+              transform: "translateX(-50%)",
+              width: "140px",
+              zIndex: 10,
+              paddingTop: "20px",
+            }}
+          />
+          <h2 className="text-center mb-4" style={{ color: "#f7f9fc" }}>
+          Recuperar senha
+        </h2>
+        <h6 className="text-center mb-3" style={{ color: "#f7f9fc" }}>
+          Insira seu email para receber seu código de validação!
+        </h6>
+        </div>
         <form onSubmit={handleSendEmail}>
           <input
             type="email"
@@ -77,39 +95,65 @@ const RecuperacaoSenha = ({ setShowRecovery, setShowCodeVerification }) => {
             className="form-control mb-3"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            style={{
+              padding: "15px",
+              fontSize: "16px",
+              borderRadius: "8px",
+              border: "1px solid #ddd",
+              boxShadow: "0 2px 5px rgba(0, 0, 0, 0.1)",
+              width: "100%",
+              marginBottom: "10px",
+            }}
           />
           {successMessage && (
-            <div style={{ color: "green", marginBottom: "10px" }}>{successMessage}</div>
+            <div style={{ color: "green", marginBottom: "15px", fontWeight: "500" }}>
+              {successMessage}
+            </div>
           )}
           {errorMessage && (
-            <div style={{ color: "red", marginBottom: "10px" }}>{errorMessage}</div>
+            <div style={{ color: "red", marginBottom: "15px", fontWeight: "500" }}>
+              {errorMessage}
+            </div>
           )}
           <button
-            className="btn w-100"
+            type="submit"
             style={{
+              width: "100%",
               backgroundColor: "#8fdcbc",
               color: "#fff",
-              padding: "10px",
+              padding: "12px",
               fontWeight: "bold",
               borderRadius: "8px",
+              border: "none",
+              cursor: "pointer",
+              transition: "all 0.3s ease",
             }}
+            onMouseOver={(e) => (e.target.style.backgroundColor = "#7bbda1")}
+            onMouseOut={(e) => (e.target.style.backgroundColor = "#8fdcbc")}
           >
             Enviar Código
           </button>
         </form>
-        <button
-          className="btn w-100 mt-2"
-          style={{
-            backgroundColor: "#e2e8ed",
-            color: "#0d1b2a",
-            padding: "10px",
-            fontWeight: "bold",
-            borderRadius: "8px",
-          }}
-          onClick={() => navigate('/login')} // Fechar a tela de recuperação
-        >
-          Cancelar
-        </button>
+        <div style={{ textAlign: "center", marginTop: "15px" }}>
+          <button
+            className="btn w-100"
+            style={{
+              backgroundColor: "#fff",
+              color: "#0d1b2a",
+              padding: "12px",
+              fontWeight: "bold",
+              borderRadius: "8px",
+              border: "none",
+              cursor: "pointer",
+              transition: "all 0.3s ease",
+            }}
+            onClick={() => navigate('/login')}
+            onMouseOver={(e) => (e.target.style.backgroundColor = "#f4f4f4")}
+            onMouseOut={(e) => (e.target.style.backgroundColor = "#fff")}
+          >
+            Cancelar
+          </button>
+        </div>
       </div>
     </div>
   );
